128da006e69784028d89862ab609bf0236a74587
Xaver <xavergig@gmail.com>
2026-03-15 12:15:07 +1100

    Add LTE Admin theme integration
    - Install django-adminlte3 package
    - Add adminlte3 and adminlte3_theme to INSTALLED_APPS
    - Update base template to extend adminlte/base.html
    - Redesign login page with LTE Admin styling and icons
    - Create comprehensive dashboard with sidebar, widgets, and user info
    - Add static files configuration
    - Ignore staticfiles/ directory to prevent commit issues
    - All tests passing (7/7)

--- hc_project/settings.py
+++ hc_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lamb
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlte3',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Login/Logout URLs
 LOGIN_URL = 'authentication:login'
